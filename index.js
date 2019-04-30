8d4cab1c50c0098de5c6eb121bc10b57d005bfd4
Matt Werner <mrwerner392@gmail.com>
2019-04-30 15:52:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-// return the first element that matches a selectors
+// return the first element that matches a selector
 function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
 
+// returns an element by ID "nested" and class name "target"
 function nestedTarget() {
   return document.getElementById('nested').getElementsByClassName('target')[0];
+}
+
+// increases ranks in all .ranked-list by n
+function increaseRankBy(n) {
+  const rankedList = document.getElementsByClassName('ranked-list');
+  for (let i = 0; i < rankedList.length; i++) {
+    let int = parseInt(rankedList[i]);
+    rankedList[i] = int + n;
+  }
 }
