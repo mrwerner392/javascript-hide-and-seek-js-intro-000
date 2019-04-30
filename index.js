c3f53c2accc57bae2510cadc49641fb3bab0579f
Matt Werner <mrwerner392@gmail.com>
2019-04-30 16:26:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+// return the first element that matches a selector
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+// returns an element by ID "nested" and class name "target"
+function nestedTarget() {
+  return document.getElementById('nested').getElementsByClassName('target')[0];
+}
+
+// increases ranks in all .ranked-list by n
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list li');
+  for (let i = 0; i < rankedLists.length; i++) {
+    let int = parseInt(rankedLists[i].innerHTML);
+    rankedLists[i].innerHTML = int + n;
+  }
+}
+
+// returns the most deeply nested child in #grand-node
+function deepestChild() {
+  let current = document.querySelector('#grand-node');
+  while (current) {
+    if (!current.children[0]) {
+      return current;
+    }
+    current = current.children[0];
+  }
+}
